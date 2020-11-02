eeda04346dcb050f6b8cd8b7610885131157c3d1
jake <www.1240035451@qq.com>
2020-11-02 10:07:52 +0800

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','StaticPagesController@home');
+Route::get('/','StaticPagesController@home')->name('home');
 
-Route::get('/test','StaticPagesController@test');
+Route::get('/help','StaticPagesController@help')->name('help');
 
-Route::get('/help','StaticPagesController@help');
-
-Route::get('/about','StaticPagesController@about');
+Route::get('/about','StaticPagesController@about')->name('about');
